626fdd6db94951a117cad6d2556230402c16832f
AhmadGitacc <132521399+AhmadGitacc@users.noreply.github.com>
2023-06-07 16:38:00 +0100

    Add files via upload

---
+++ AMDHealth/js/disease.js
@@ -0,0 +1,11 @@
+function searchDisease() {
+    let searched_Disease = document.getElementById("search").value;
+    let disease_Info = document.getElementById(searched_Disease);
+    let disease = document.getElementById("infodiv");
+
+    if (disease_Info) {
+        disease.innerHTML = disease_Info.innerHTML;
+    } else {
+        disease.innerHTML = "Disease information not currently available.";
+    }
+}
